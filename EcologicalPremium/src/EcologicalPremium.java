c8b8098c78a77ba4008b2b8aba12b44874c87986
Austin Klum <Austin Klum@DESKTOP-DCSDHIN>
2017-09-27 17:12:30 -0500

    A simple loop program eco * land

--- EcologicalPremium/src/EcologicalPremium.java
+++ EcologicalPremium/src/EcologicalPremium.java
@@ -1,9 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class EcologicalPremium {
 
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(new file("SampleInput.txt"));
-		//Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws FileNotFoundException {
+		@SuppressWarnings("resource")
+		//Scanner scan = new Scanner(new File("SampleInput.txt"));
+		Scanner scan = new Scanner(System.in);
+		
+		int numCases = scan.nextInt();
+		for(int i = 0; i < numCases; i++) {
+			int farmers = scan.nextInt();
+			int moneyRecieved = 0;
+			for(int j = 0; j < farmers; j++) {
+				int size = scan.nextInt(), animals = scan.nextInt(), evoFri = scan.nextInt();
+				moneyRecieved += size * evoFri;
+			}
+			System.out.println(moneyRecieved);
+		}
 	}
 
 }
