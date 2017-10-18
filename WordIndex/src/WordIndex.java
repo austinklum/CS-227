9e1363fc9bbcebd87a611d37c94d0c8be8c8f040
Austin Klum <Austin Klum@DESKTOP-DCSDHIN>
2017-10-18 18:31:59 -0500

    Doesnt work

---
+++ WordIndex/src/WordIndex.java
@@ -0,0 +1,34 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class WordIndex {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		//Sacnner scan = new Scanner(System.in);
+		Scanner scan = new Scanner(new File("input.txt"));
+		
+		outerLoop:
+		while(scan.hasNextLine()) {
+			String str = scan.nextLine();
+			//Check again invalid inputs
+			for(int i = 1; i < str.length(); i++) {
+				char max = str.charAt(0);
+				if(max >= str.charAt(i)) {
+					System.out.println("0");
+					break outerLoop;
+				}
+			}
+			int repeatCount = 0;
+			int total = 0;
+			int arr[] = {0,26,25,24};
+			for(int i = 0; i < str.length(); i++) {
+				int c = str.charAt(i) - 'a';
+				total += c + ( arr[i] * 26);
+				
+			}
+			
+		}
+	}
+
+}
