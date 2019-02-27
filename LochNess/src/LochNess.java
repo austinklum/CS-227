859c94a803d429fe613f85ad2a3167e0986af697
Austin Klum <klumaustin@gmail.com>
2019-02-27 17:34:11 -0600

    Finished loch ness

--- LochNess/src/LochNess.java
+++ LochNess/src/LochNess.java
@@ -11,8 +11,15 @@ import java.util.Scanner;
 
 public class LochNess {
 	public static void main(String[] args) throws FileNotFoundException {
-		//Scanner scan = new Scanner(System.in);
-		Scanner scan = new Scanner(new File("in"));
+		Scanner scan = new Scanner(System.in);
+		//Scanner scan = new Scanner(new File("in"));
+		int t = scan.nextInt();
+		
+		for(int i = 0; i < t;i++) {
+			int rows = scan.nextInt();
+			int cols = scan.nextInt();
 			
+			System.out.println((int)Math.ceil((rows/3) * (cols/3)));
+		}
 	}
 }
