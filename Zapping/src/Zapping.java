aac07a2e8e4a91ba877ff709ccd34a0b61aad45d
Austin Klum <klumaustin@gmail.com>
2019-03-06 17:26:21 -0600

    Finished Zapping

--- Zapping/src/Zapping.java
+++ Zapping/src/Zapping.java
@@ -12,8 +12,23 @@ import java.util.Scanner;
 
 public class Zapping {
 	public static void main(String[] args) throws FileNotFoundException {
-		//Scanner scan = new Scanner(System.in);
-		Scanner scan = new Scanner(new File("in"));
-		
+		Scanner scan = new Scanner(System.in);
+		//Scanner scan = new Scanner(new File("in"));
+		int start = scan.nextInt();
+		int end = scan.nextInt();
+		int first = 0;
+		int second = 0;
+		while(start > -1 && end > -1) {
+		    if (start > end) {
+		    	first = start - end;
+	            second = end + 100 - start;
+	        } else {
+	    	  first = end - start;
+	    	  second = start + 100 - end;
+	        }
+		    System.out.println(Math.min(first, second));
+			start = scan.nextInt();
+			end = scan.nextInt();
+		}
 	}
 }
