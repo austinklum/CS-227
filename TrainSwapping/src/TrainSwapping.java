de6d45e799095a0cfcc72563ba705107570eec61
Austin Klum <klumaustin@gmail.com>
2019-03-06 17:30:48 -0600

    Inital commit for Train Swapping

---
+++ TrainSwapping/src/TrainSwapping.java
@@ -0,0 +1,18 @@
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ * 
+ * @author Austin Klum
+ * CM Contest Problem Archive 299 Train Swapping
+ * 2019/03/06
+ * 
+ */
+
+public class TrainSwapping {
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner scan = new Scanner(System.in);
+		//Scanner scan = new Scanner(new File("in"));
+		
+	}
+}
