e87c5500dbc1dd1e9134707b1fb5ea156c8fff98
Austin Klum <klumaustin@gmail.com>
2019-03-27 18:10:36 -0500

    Wine Trading is finished

--- WineTrading/src/WineTrading.java
+++ WineTrading/src/WineTrading.java
@@ -4,15 +4,23 @@ import java.util.Scanner;
 
 public class WineTrading {
 	public static void main(String[] args) throws FileNotFoundException {
-//		Scanner scan = new Scanner(System.in);
-		Scanner scan = new Scanner(new File("in"));
+        Scanner scan = new Scanner(System.in);
+	//	Scanner scan = new Scanner(new File("in"));
 		
-		while(scan.hasNext()) {
-			int next = scan.nextInt();
-			while(next > 0) {
-				
-				next = scan.nextInt();
+		int next = scan.nextInt();
+		while(next > 0) {
+			long total = 0;
+			long work = 0;
+			for (int i = 0; i < next; i++) {
+                total += scan.nextInt();
+                if(total < 0){
+                    work -= total;
+                } else {
+                    work += total;
+                }
 			}
+			System.out.println(work);
+			next = scan.nextInt();
 		}
 	}
 }
