05d536a9b800fba25d72f31fd9d82143e6ddf205
Austin Klum <klumaustin@gmail.com>
2019-03-27 16:42:12 -0500

    Init commit for wine Trading

---
+++ WineTrading/src/WineTrading.java
@@ -0,0 +1,18 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class WineTrading {
+	public static void main(String[] args) throws FileNotFoundException {
+//		Scanner scan = new Scanner(System.in);
+		Scanner scan = new Scanner(new File("in"));
+		
+		while(scan.hasNext()) {
+			int next = scan.nextInt();
+			while(next > 0) {
+				
+				next = scan.nextInt();
+			}
+		}
+	}
+}
